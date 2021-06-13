d0e72d07f14c43c517f24c434aea9f0d4561aef0
joong0818 <77487959+joong0818@users.noreply.github.com>
2021-06-13 23:17:46 +0900

    계속 업데이트 할 예정

---
+++ My page/script.js
@@ -0,0 +1,16 @@
+function book() {
+    location.href='독서.html';
+}
+
+function Ocean() {
+    location.href='바다.html';
+}
+
+function Web() {
+    location.href='웹.html';
+}
+
+function What() {
+    alert("Oops! It's not ready!");
+}
+
