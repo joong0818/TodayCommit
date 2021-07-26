6a1574cf4f7d24a65defa88a7980c9544a6e8ae0
joong0818 <77487959+joong0818@users.noreply.github.com>
2021-07-26 23:44:28 +0900

    Add files via upload

---
+++ 자바스크립트/오늘 날짜.js
@@ -0,0 +1,9 @@
+today = new Date();
+
+document.write("<center>");
+document.write("오늘 날짜를 출력합니다.<br><br>");
+document.write( today.getYear() + "년 ") ;
+document.write( (today.getMonth()+1) + "월 ");
+
+document.write( today.getDate() + "일 입니다.<HR>");
+
