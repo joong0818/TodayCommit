189f35b5ab47c15ab11a806026b84b6efd0a5b24
joong0818 <77487959+joong0818@users.noreply.github.com>
2021-07-29 22:59:08 +0900

    Add files via upload

---
+++ Array 객체.js
@@ -0,0 +1,12 @@
+a = new Array(5)
+
+a[0] = 10
+a[1] = 20
+a[2] = 30
+a[3] = 40
+a[4] = 50
+
+document.write("<center><h3>");
+for( i = 0 ; i < 5 ; i++ )
+    document.write("a[" + i + "]=" + a[i] + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
+Document.write("</h3></center>")
