137e236c8ff3cb423d086f2c1fd9704a4aa7745a
joong0818 <77487959+joong0818@users.noreply.github.com>
2021-07-30 23:51:29 +0900

    하이퍼링크 만들어보기

---
+++ 자바스크립트/link 메소드.js
@@ -0,0 +1,9 @@
+document.write("<center>")
+document.write("<h3> * string 객체 * - 검 색 엔 진 <hr>")
+document.write("<br><br>")
+document.write("■ 네 이 버 ■" .link("http://www.naver.com"));
+document.write("<br><br>")
+document.write("■ 구글 ■" .link("http://www.google.com"));
+document.write("<br><br>")
+document.write("■ 다음 ■" .link("http://www.daum.net"));
+document.write("</center>")
