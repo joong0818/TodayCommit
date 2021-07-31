20d3436b0823d014e4c85c3cdc4a7f7f6d1b5ea7
joong0818 <77487959+joong0818@users.noreply.github.com>
2021-07-31 23:36:45 +0900

    Add files via upload

---
+++ 자바스크립트/anchor 메소드.js
@@ -0,0 +1,6 @@
+document.write("<center>");
+document.write("<br><br> ■ 문서의 시작 부분 ■".anchor("top"));
+document.write("<br><br><img src=' ./성백1.jpg'>");
+document.write("<br><br><img src=' ./성백2.jpg'>");
+document.write("<br><br> ■ 문서의 시작 부분으로 이동합니다. ■".link("#top"));
+document.write("</center>")
