288c9882f2b0e6684e9cace7e1d9b0f66e64c6c0
joong0818 <77487959+joong0818@users.noreply.github.com>
2021-08-01 23:49:55 +0900

    Add files via upload

---
+++ 자바스크립트/charAt 메소드.js
@@ -0,0 +1,7 @@
+src = "friend"
+document.write("<center><h2>");
+document.write(str+" 의 길이 : " + str.length + "<hr>");
+for(i=0; i<str.length; i++)
+    document.write(str.charat(i) + " ");
+
+document.write("</h2></center>")
