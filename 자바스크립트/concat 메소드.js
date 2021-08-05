3408d97ef0d5670d84ce25328b63b53ebf9d97fb
joong0818 <77487959+joong0818@users.noreply.github.com>
2021-08-05 23:41:14 +0900

    Add files via upload

---
+++ 자바스크립트/concat 메소드.js
@@ -0,0 +1,9 @@
+str1 = '행복해지는'
+str2 = 'HTML'
+str3 = str1.concat(str2)
+
+document.write("<center><h2>")
+document.write(str1+"<hr>")
+document.write(str2+"<hr>")
+document.write(str3)
+document.write("</h2></center>")
