2324f0ebd7b549cf8b89ddd31dfbe45157c58d01
joong0818 <77487959+joong0818@users.noreply.github.com>
2021-08-23 23:50:20 +0900

    Add files via upload

---
+++ bgColor 속성.js
@@ -0,0 +1,7 @@
+color = new Array('red','orange','yellow','green','blue','navy','violet')
+function chbgColor(index){
+    document.bgColor = color[index]
+}
+function init() {
+    window.resizeTo(350,550)
+}
