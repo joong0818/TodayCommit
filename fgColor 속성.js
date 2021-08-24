6b2dfb428c712f8c2010e05e3a98410df60828ef
joong0818 <77487959+joong0818@users.noreply.github.com>
2021-08-24 23:10:04 +0900

    Add files via upload

---
+++ fgColor 속성.js
@@ -0,0 +1,7 @@
+color = new Array('red','orange','yellow','green','blue','navy','violet')
+function chbgColor(index){
+    document.fgColor = color[index]
+}
+function init() {
+    window.resizeTo(720,300)
+}
