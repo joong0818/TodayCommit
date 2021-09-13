f5bdea4c53e4473f53596d32d1ddf5b5ab056863
joong0818 <77487959+joong0818@users.noreply.github.com>
2021-09-13 17:12:18 +0900

    자바스크립트 - 변수
    전역변수와 지역변수의 차이

---
+++ 변수 차이/외부 스크립트.js
@@ -0,0 +1,14 @@
+var x = document.getElementById('menu').style;
+var y = 12;
+
+function A(){
+    x.height='200px';
+                
+
+
+    }
+            
+function B(){
+    
+    alert(y);
+    }
